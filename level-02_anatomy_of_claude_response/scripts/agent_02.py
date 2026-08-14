a28a4bc25f80b82921fd637b577bb0957dd94b14
malirakesh <rakesh.mali80@gmail.com>
2026-08-13 21:25:33 -0400

    claude api response in detail

---
+++ level-02_anatomy_of_claude_response/scripts/agent_02.py
@@ -0,0 +1,42 @@
+
+# Author: Rakesh Kumar Mali
+
+# load environment variables from .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+# load the anthropic library
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-haiku-4-5-20251001"
+
+# create a prompt for the model. 
+# Make a request to the model with the prompt and print the RAW response received from the Claude model.
+# This gives you the opportunity to see the raw response and understand how to parse it for your needs and play around it.
+message = client.messages.create(
+    model=model,                    # this parameter is used to declare which model to use for the request.
+    max_tokens=1024,                # this parameter is used to tell the claude model the maximum number of tokens to generate in the response.
+    messages=[                      # this parameter is used to provide the messages to the claude model. The messages are provided in a list of dictionaries, where each dictionary represents a message. Each message has a role and content. The role can be either "user" or "assistant", and the content is YOUR prompt/query to the model.
+        {
+            "role": "user", 
+            "content": "What is the capital of France?"
+        }
+    ]
+)
+print(message)                      # this simply prints the raw response received from the Claude model. You can parse this response to extract the information you need.
+
+##  UNTIL THIS POINT, IT IS THE SAME AS THE PREVIOUS level-01 SCRIPT. 
+##  NOW, LET'S PRINT EACH OF THE ATTRIBUTES OF THE RESPONSE AND UNDERSTAND THEIR MEANING.
+
+print("Response ID:", message.id)  # this prints the unique identifier of the response received from the Claude model. This can be useful for tracking and referencing specific responses.
+print("Response Object:", message.container)  # this prints the type of container
+print("Response Citations:", message.content[0].citations)
+print("Response Text:", message.content[0].text)
+print("Response Model:", message.model)
+print("Response Role:", message.role)
+print("Response Stop_Reason:", message.stop_reason)
+print("Response Stop_Details:", message.stop_details)
+print("Response Stop_Sequence:", message.stop_sequence)
+print("Response Type:", message.type)
+print("Response Usage:", message.usage)
